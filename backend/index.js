9cde726d5d2a815e23211ff8a42929f153472483
sheeba-ansari <ansarisheeba5000@gmail.com>
2026-04-26 21:21:31 +0530

    added env variables

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const app = express();
-
 // Use environment variable for PORT
 const PORT = process.env.PORT || 3000;
-
 // Example environment variable (dummy)
 const SECRET = process.env.JWT_SECRET || "defaultsecret";
-
 app.get("/", (req, res) => {
-  res.send("Backend is running with env variables!");
+ res.send("Backend is running with env variables");
 });
-
 app.get("/secret", (req, res) => {
-  res.send("Secret is: " + SECRET);
+ res.send("Secret is: " + SECRET);
 });
-
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+ console.log("Server running on port " + PORT);
 });
